3dd85e9e5c7c63ee869a030a16ac1b4f4fb8e626
BuildTools <unconfigured@null.spigotmc.org>
2023-01-11 02:13:04 +0100

    Cleaned up main class + added global boolean chatEnabled for chattoggle command

--- src/main/java/me/r4wpanda/chatmanager/ChatManagerPlugin.java
+++ src/main/java/me/r4wpanda/chatmanager/ChatManagerPlugin.java
@@ -5,20 +5,43 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ChatManagerPlugin extends JavaPlugin {
 
+    public boolean chatEnabled;
+
     @Override
     public void onEnable() {
+
+        chatEnabled = true;
 
         mkDefDir();
         getConfig().options().copyDefaults();
         saveDefaultConfig();
-        CensoredConfigManager CCManager = new CensoredConfigManager(this);
 
-        Bukkit.getPluginManager().registerEvents(new ChatListener(CCManager), this);
+        CensoredConfigManager CCManager = new CensoredConfigManager(this);;
+        registerOther(CCManager);
+        registerCommands();
+        registerEvents(CCManager);
 
 
     }
 
 
+    public void setChatEnabled(boolean chatEnabled) {
+        this.chatEnabled = chatEnabled;
+    }
+
+    public void registerOther(CensoredConfigManager CCManager) {
+        new Utils(CCManager);
+    }
+
+    public void registerCommands() {
+        ChatToggle chatToggle = new ChatToggle(this);
+        getCommand("chattoggle").setExecutor(chatToggle);
+        getCommand("chattoggle").setTabCompleter(chatToggle);
+    }
+
+    public void registerEvents(CensoredConfigManager CCManager) {
+        Bukkit.getPluginManager().registerEvents(new ChatListener(CCManager), this);
+    }
 
     public void mkDefDir() {
         if (!getDataFolder().exists()) {
